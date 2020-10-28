d6df00d8d5586b538c5fd6256c5f9c6d2f93943f
Andrew Liew <72509456+andrewliewkk@users.noreply.github.com>
2020-10-28 10:19:15 +0800

    Added keypress functionality

--- index.js
+++ index.js
@@ -2,10 +2,45 @@ var buttons = document.querySelectorAll(".drum");
 
 
 for (const button of buttons) {
-    button.addEventListener("click", play);
+    button.addEventListener("click", function(){
+        makeSound(button.innerHTML);
+    });
 }
 
-function play() {
-    var audio = new Audio("sounds/crash.mp3");
-    audio.play();
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default:
+    }
 }
